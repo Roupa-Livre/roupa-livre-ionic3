5b62b361899e2e4e6353b403e85d89dcfbbf7aa7
Miguel Ayoub <mayoub@miguels-macbook.home>
2018-03-19 06:04:12 -0300

    Padronizacao de identacao dos Components

--- src/components/demo-avatar/demo-avatar.ts
+++ src/components/demo-avatar/demo-avatar.ts
@@ -1,31 +1,24 @@
 import { Component, Input } from '@angular/core';
 
-/**
- * Generated class for the DemoAvatarComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
 @Component({
-  selector: 'demo-avatar',
-  templateUrl: 'demo-avatar.html'
+	selector: 'demo-avatar',
+	templateUrl: 'demo-avatar.html'
 })
 export class DemoAvatarComponent {
-  @Input() sizeClass: string = 'thumb';// Can be 'thumb-xs'-'thumb-sm'-'thumb-md'-'thumb-lg'
+	@Input() sizeClass: string = 'thumb';// Can be 'thumb-xs'-'thumb-sm'-'thumb-md'-'thumb-lg'
 
-  images: [any] = [
-    'adam.png',
-    'ben.png',
-    'hieu.png',
-    'max.png',
-    'mike.png',
-    'perry.png'
-  ];
-  imageUrl: string;
+	images: [any] = [
+		'adam.png',
+		'ben.png',
+		'hieu.png',
+		'max.png',
+		'mike.png',
+		'perry.png'
+	];
+	imageUrl: string;
 
-  constructor() {
-    let randomIndex = Math.floor(Math.random() * (this.images.length - 1));
-    this.imageUrl = `assets/img/${this.images[randomIndex]}`;
-
-  }
+	constructor() {
+		let randomIndex = Math.floor(Math.random() * (this.images.length - 1));
+		this.imageUrl = `assets/img/${this.images[randomIndex]}`;
+	}
 }
--- src/components/giphy/nlbr.pipe.ts
+++ src/components/giphy/nlbr.pipe.ts
@@ -6,12 +6,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 */
 
 @Pipe({
-  name: 'nlbr'
+	name: 'nlbr'
 })
 export class NlbrPipe implements PipeTransform {
-  transform(value: string) {
-    if (!value) return value;
+	transform(value: string) {
+		if (!value) return value;
 
-    return value.replace(/\n/ig, '<br>');
-  }
+		return value.replace(/\n/ig, '<br>');
+	}
 }
