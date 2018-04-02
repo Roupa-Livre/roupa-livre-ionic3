625b0e61e1800a444347976318e7c109da48dd5d
Miguel Ayoub <mayoub@miguels-macbook.home>
2018-04-02 08:27:26 -0300

    Edição da Tela de Lista de Mensagens

--- src/pages/messaging/messaging.ts
+++ src/pages/messaging/messaging.ts
@@ -8,16 +8,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MessagingPage {
 
+	// CONSTRUCTOR
 	constructor(
-		public navCtrl: NavController, 
+		public navCtrl: NavController,
 		public navParams: NavParams
 	) {
 	}
 
+	// LIFECYCLE EVENTS
 	ionViewDidLoad() {
 		console.log('ionViewDidLoad MessagingPage');
 	}
 
+	// CLICK EVENTS
 	goToChat(isNewMatch = false) {
 		this.navCtrl.push('ChatPage', {isNewMatch: isNewMatch});
 	}
