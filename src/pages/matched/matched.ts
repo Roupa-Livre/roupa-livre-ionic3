5d47f53cea48dd246007ce6b23cef7bf99acedbf
Miguel Ayoub <mayoub@miguels-macbook.home>
2018-04-02 08:25:21 -0300

    Edição da Tela de Combinou

--- src/pages/matched/matched.ts
+++ src/pages/matched/matched.ts
@@ -8,18 +8,28 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class MatchedPage {
 
+	// CONSTRUCTOR
 	constructor(
-		public navCtrl: NavController, 
-		public navParams: NavParams, 
+		public navCtrl: NavController,
+		public navParams: NavParams,
 		public viewCtrl: ViewController
 	) {
 	}
 
+	// LIFECYCLE EVENTS
 	ionViewDidLoad() {
-		console.log('ionViewDidLoad MatchedPage');
+		console.log('MATCHED-PAGE - IONVIEWDIDLOAD');
+	}
+
+	// CLICK EVENTS
+	goToChat() {
+		console.log('MATCHED-PAGE - GOTOCHAT');
+		this.viewCtrl.dismiss();
+		this.navCtrl.push('ChatPage', { 'isNewMatch': true });
 	}
 
 	close() {
+		console.log('MATCHED-PAGE - CLOSE');
 		this.viewCtrl.dismiss();
 	}
 }
