0088e54271c8f822116c2bf51f7b735d02f7a231
Miguel Ayoub <miguel.ayoub@ccee.org.br>
2019-06-01 18:41:24 -0300

    PermissionLocationPage - Criação de Tela com Layout Atualizado

---
+++ src/pages/permission-location/permission-location.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-permission-location',
+  templateUrl: 'permission-location.html',
+})
+export class PermissionLocationPage {
+
+  // CONSTRUCTOR
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams
+  ) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PermissionLocationPage');
+  }
+
+  // CLICK EVENTS
+  activeLocation() {
+    console.log("PERMISSION LOCATION - ACTIVE LOCATION");
+    // TODO : ACTIVATE THE LOCATION
+    this.navCtrl.push("PermissionNotificationPage");
+  }
+
+  denyLocation() {
+    console.log("PERMISSION LOCATION - DENY LOCATION");
+    this.navCtrl.push("PermissionNotificationPage");
+  }
+
+}
