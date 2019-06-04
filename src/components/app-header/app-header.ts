47f68365e09e988e06f830fd2b84aaa91dd4f6f3
Miguel Ayoub <ayoubmr@gmail.com>
2019-06-04 01:33:40 -0300

    Adição de Pins para o AppHeaderComponent

--- src/components/app-header/app-header.ts
+++ src/components/app-header/app-header.ts
@@ -9,6 +9,10 @@ export class AppHeaderComponent {
 
   @Input() activeView: string;
 
+  @Input() profilePush: boolean;
+  @Input() chatPush: boolean;
+
+
   // @Output() mainClicked = new EventEmitter();
   // @Output() profileClicked = new EventEmitter();
   // @Output() chatClicked = new EventEmitter();
@@ -18,6 +22,19 @@ export class AppHeaderComponent {
     public navCtrl: NavController,
   ) {
     console.log('Hello AppHeaderComponent Component');
+
+    if (!this.activeView) {
+      this.activeView = "main";
+    }
+
+    // TODO : IMPLEMENTAR MODO DE OBTER SE OS INDICADORES ESTÃO ATIVOS OU NÃO
+    if (!this.profilePush) {
+      this.profilePush = true;
+    }
+
+    if (!this.chatPush) {
+      this.chatPush = false;
+    }
   }
 
   // CLICK METHODS
