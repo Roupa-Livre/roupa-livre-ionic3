f45398c66a5cf66256083f0a7577e02e121837e6
Henrique Rangel <hrangel@nucleo235.com.br>
2019-09-24 19:20:25 -0300

    Configurada tela de what you release

--- src/pages/what-you-release/what-you-release.ts
+++ src/pages/what-you-release/what-you-release.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PropertyGroupService } from '../../services/property-group-service';
+
+const allowedCategories = ["acessory", "clothing", "shoes", "purse"];
 
 @IonicPage()
 @Component({
@@ -8,21 +11,39 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class WhatYouReleasePage {
 
+  groups = [];
+  selectedGroup = null;
+
   // CONSTRUCTOR
-  constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private propertyGroupService: PropertyGroupService,
   ) {
+    this.loadFirstTime();
+  }
+
+  async loadFirstTime() {
+    const rootGroups: any[] = await this.propertyGroupService.root();
+    if (rootGroups.length > 0) {
+      const groups = [];
+      for (const group of rootGroups[0].properties) {
+        if (allowedCategories.indexOf(group.code) > -1) {
+          groups.push(group);
+        }
+      }
+      this.groups = groups;
+    }
   }
 
   // LIFECYCLE EVENTS
   ionViewDidLoad() {
+    this.loadFirstTime();
     console.log('ionViewDidLoad WhatYouReleasePage');
   }
 
   // CLICK EVENTS
-  chooseReleaseOption() {
+  chooseReleaseOption(option) {
     console.log("WHATYOURELEASEPAGE - CHOOSERELEASEOPTION");
+    this.selectedGroup = option;
   }
 
   releaseApparel() {
