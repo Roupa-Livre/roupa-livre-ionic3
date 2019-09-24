5a9e16ccfb2fa9146490ac1ea6e300ca535d15c7
Henrique Rangel <hrangel@nucleo235.com.br>
2019-09-24 10:45:51 -0300

    Adicionadas validacoes ao fazer cadastro

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -4,6 +4,9 @@ import { RegisteringUser } from '../../models/user';
 import { LoginServiceProvider } from '../../services/login-service';
 import { ToastService } from '../../services/toast-service';
 
+const emailRegex =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 @IonicPage()
 @Component({
   selector: 'page-register',
@@ -31,8 +34,25 @@ export class RegisterPage {
   // CLICK EVENTS
   async register() {
     try {
+      if (this.user.password != this.user.passwordConfirmation) {
+        this.toast.showError(`Senha e confirmação devem ser as mesmas`);
+        return;
+      }
+      if (!this.user.password || this.user.password.length < 8) {
+        this.toast.showError(`Senha deve ter pelo menos 8 caracteres`);
+        return;
+      }
+      if (!this.user.name || this.user.name.length < 2) {
+        this.toast.showError(`Nome não preenchido corretamente`);
+        return;
+      }
+      if (!this.user.login || !emailRegex.exec(this.user.login)) {
+        this.toast.showError(`E-mail não preenchido corretamente`);
+        return;
+      }
+
       this.user.type = 'KindHeartedUser';
-      const result = await this.loginProvider.register(this.user);
+      await this.loginProvider.register(this.user);
 
       this.toast.showMessage(`Oba! Que bom que você se conectou com a gente!`);
 
